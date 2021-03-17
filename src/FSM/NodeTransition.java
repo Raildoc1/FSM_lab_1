7e5bc04fe5852675f64c5fcae762041253f27937
Ivan Timofeev <alexrush1@yandex.ru>
2021-03-17 15:51:48 +0700

    i'm trying :c

--- src/FSM/NodeTransition.java
+++ src/FSM/NodeTransition.java
@@ -3,6 +3,15 @@ package FSM;
 import Operations.ITreeNode;
 
 public class NodeTransition {
+
+    public NodeTransition(int start, int end, char condition, ITreeNode treeCondition, boolean isEpsilon) {
+        this.start = start;
+        this.end = end;
+        this.condition = condition;
+        this.treeCondition = treeCondition;
+        this.isEpsilon = isEpsilon;
+    }
+
     public int start;
     public int end;
     public char condition;
--- src/TransistTable.java
+++ src/TransistTable.java
@@ -1,24 +1,52 @@
+import FSM.NodeTransition;
 import Operations.Add;
 import Operations.ITreeNode;
 import Operations.Loop;
 import Operations.Multiply;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class TransistTable {
+    int lastState = 0;
+    Stack<NodeTransition> nodes = new Stack<>();
 
-    public void toTransistTable(ITreeNode tree) {
-        //some cycle
-        var leaf = tree.getLeft();
+    public TransistTable(ITreeNode tree) {
+        nodes.add(new NodeTransition(lastState, lastState + 1, ' ', tree, false));
+        lastState++;
+    }
 
-        if (leaf != null) {
-            toTransistTable(leaf);
-        }
+    public void toTransistTable() {
+        NodeTransition currentNode = nodes.pop();
 
-        if (leaf instanceof Add) {
-            //some code
-        } else if (leaf instanceof Multiply) {
-            //some code
-        } else if (leaf instanceof Loop) {
-            //some code
+        if (currentNode.treeCondition instanceof Add) {
+            if (currentNode.treeCondition.getChar() != ' ') {
+                //i dnt knw
+                nodes.add(new NodeTransition(lastState + 1, currentNode.end, currentNode.treeCondition.getChar(), null, false));
+            } else {
+                nodes.add(new NodeTransition(currentNode.start, currentNode.end, ' ', currentNode.treeCondition.getLeft(), false));
+                lastState++;
+                nodes.add(new NodeTransition(currentNode.start, currentNode.end, ' ', currentNode.treeCondition.getRight(), false));
+            }
+            lastState++;
+        } else if (currentNode.treeCondition instanceof Multiply) {
+            if (currentNode.treeCondition.getChar() != ' ') {
+                //i dnt knw
+                nodes.add(new NodeTransition(lastState + 1, currentNode.end, currentNode.treeCondition.getChar(), null, false));
+            } else {
+                nodes.add(new NodeTransition(currentNode.start, lastState + 1, ' ', currentNode.treeCondition.getLeft(), false));
+                lastState++;
+                nodes.add(new NodeTransition(lastState + 1, currentNode.end, ' ', currentNode.treeCondition.getRight(), false));
+            }
+            lastState++;
+        } else if (currentNode.treeCondition instanceof Loop) {
+            if (currentNode.treeCondition.getChar() != ' ') {
+                //i dnt knw
+                nodes.add(new NodeTransition(lastState + 1, currentNode.end, currentNode.treeCondition.getChar(), null, true));
+            } else {
+                //some code
+            }
+            lastState++;
         }
 
     }
